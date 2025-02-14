fd0cb099967dcd61ab1f583cf901db2b2493100b
Kian Seong <jaylks96@gmail.com>
2025-02-15 00:03:56 +0800

    - implemented neural network tracking method - refactored to make repetitive codes into a common functions

--- HW1/01_Single_Object_Tracking/TemplateMatchingTracker.py
+++ HW1/01_Single_Object_Tracking/TemplateMatchingTracker.py
@@ -1,4 +1,3 @@
-import os
 import cv2
 from AbstractObjectTracker import AbstractObjectTraker
 
@@ -14,8 +13,12 @@ class TemplateMatchingTracker(AbstractObjectTraker):
     }
 
     def __init__(self, template=None, mode='sqdiff'):
-        super().__init__(template)
+        super().__init__()
+        self.set_template(template)
         self.set_mode(mode)
+
+    def set_template(self, template):
+        self._template = template
 
     def set_mode(self, mode):
         mode = mode.lower()
@@ -26,12 +29,14 @@ class TemplateMatchingTracker(AbstractObjectTraker):
 
         self._mode = self._TM_MODES[mode]
 
+    def list_mode(self):
+        return list(self._TM_MODES.keys())
+
     def track(self, frame):
         if self._template is None:
             print('[ERROR] No template provided')
             return
 
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         result = cv2.matchTemplate(frame, templ=self._template, method=self._mode)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
@@ -51,44 +56,13 @@ class TemplateMatchingTracker(AbstractObjectTraker):
 
 if __name__ == '__main__':
     import argparse as ap
-    import glob
-    import matplotlib.pyplot as plt
+    from TemplateMatchingTracker import TemplateMatchingTracker
+    from run import run
+
     parser = ap.ArgumentParser()
     parser.add_argument('--seq', '-s', type=int, required=True)
     parser.add_argument('--mode', '-m', required=True)
     args = parser.parse_args()
 
-    mode = args.mode.strip()
-
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-
-    # Paths of images and template bounding box data
-    data_folder = f'{script_dir}/data/seq{args.seq}'
-    images_fp = sorted(glob.glob(os.path.join(data_folder, 'img/*.jpg')))
-    firsttrack_fp = f'{data_folder}/firsttrack.txt'
-    groundtruth_fp = f'{data_folder}/groundtruth.txt'
-
-    # Results path
-    results_folder = f'{script_dir}/results/1_template_matching/{mode}'
-    if not os.path.isdir(results_folder):
-        os.makedirs(results_folder)
-
-    result_fp = f'{results_folder}/trackresults_TM_seq{args.seq}.txt'
-
-    # Crop the template object
-    with open(firsttrack_fp, 'r') as f:
-        x, y, w, h = list(map(lambda n: int(n.strip()), f.readline().split(',')))
-        first_frame = cv2.cvtColor(cv2.imread(images_fp[0]), cv2.COLOR_BGR2RGB)
-        template = first_frame[y:y+h, x:x+w, :]
-        # first_frame = cv2.cvtColor(cv2.imread(images_fp[0]), cv2.COLOR_BGR2GRAY)
-        # template = first_frame[y:y+h, x:x+w]
-        # plt.imshow(template)
-        # plt.show()
-
-    tracker = TemplateMatchingTracker(template, mode)
-    tracker.track_seq(images_fp)
-    tracker.save_track_bbox(result_fp)
-    tracker.save_tracking_video(results_folder, f'trackresults_TM_seq{args.seq}', groundtruth_fp)
-    precision = tracker.evaluate(groundtruth_fp, 'precision', threshold=20).mean()
-    success = tracker.evaluate(groundtruth_fp, 'success', threshold=0.5).mean()
-    print(f'Seq={args.seq}, tm mode={mode}, precision={precision}, success={success}')
+    tracker = TemplateMatchingTracker()
+    run('template-matching', tracker, args.seq, mode=args.mode.strip(), precision_thres=20, iou_thres=0.5)
